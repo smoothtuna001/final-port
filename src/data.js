10f688723d87abf649b905209092800ae0054f77
elliott-rosenberg <elliott_rosenberg@brown.edu>
2022-12-15 11:21:45 -0500

    finished

---
+++ src/data.js
@@ -0,0 +1,62 @@
+export const projects = [
+  {
+    title: "Books on the Square",
+    subtitle: "HTML / CSS",
+    description:
+      "Redesigning the website and brand identity for a local Providence bookstore.",
+    image: "./StyleGuide_Hero.png",
+    link: "https://smoothtuna001.github.io/Redesign-Submit/",
+  },
+  {
+    title: "NFTScoring",
+    subtitle: "Brand Identity",
+    description:
+      "Building a high fidelity prototype for a pre-existing YCombinator startup.",
+    image: "./NFTScoringStyleGuideHero-01.png",
+    link: "https://jocular-bunny-cf92f1.netlify.app/",
+  },
+  {
+    title: "Daily UI",
+    subtitle: "Figma, Illustrator",
+    description:
+      "Creating digital assets as a means of practicing UI principles.",
+    image: "./Day1 - Sign In.jpg",
+    link: "https://www.elliottsrosenberg.com/daily-ui",
+  },
+  {
+    title: "Personas and Storyboarding",
+    subtitle: "UX Research",
+    description:
+      "Analyzing an interface as a means of understanding common user issues.",
+    image: "./HeroPhoto.png",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "React",
+  "HTML / CSS",
+  "Python",
+  "JavaScript",
+  "Java",
+  "Adobe Suite",
+  "Figma",
+  "Rhino3D"
+];
